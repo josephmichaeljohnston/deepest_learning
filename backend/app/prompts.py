eb4ff904072b015aa4e087a0190d33418db08bcc
Sam Armstrong <samuel_e_armstrong@yahoo.co.uk>
2025-10-18 17:25:53 +0100

    add prompts.py to backend

---
+++ backend/app/prompts.py
@@ -0,0 +1,126 @@
+def lecture_intro(student_context: str, understanding_context: str) -> str:
+    """
+    Generate a prompt for a university lecturer to give an introduction to a lecture.
+
+    Args:
+        student_context: The general hypothesis of the student (aggregated over multiple lectures)
+        understanding_context: The specific hypothesis of student's understanding of this lectures content.
+
+    Returns:
+        A prompt for a university lecturer to give an introduction to a lecture.
+    """
+    return (
+f"""
+You're a university lecturer whose giving a lecture to a student with this being the introductory slide.
+
+We're also keeping track of some extra information, to help you tailor you're lecture style. Do not mention any of this context in your response.
+
+*Context about the student you're teaching*
+<student context>
+{student_context}
+</student context>
+
+*What the student understands about this topic*
+<understanding context>
+{understanding_context}
+</understanding context>
+
+Attached is the first slide of your pdf presentation. Write a brief friendly introduction to the lecture. Make sure your keep your lecture conversational and engaging (avoid bullet points and lists where possible). Do not end your lecturing over this slide with a question or summary line.
+"""
+    )
+
+
+def lecture_step(lecture: str, student_context: str, understanding_context: str) -> str:
+    """
+    Generate a prompt for a university lecturer to continue a lecture for the next slide.
+
+    Args:
+        lecture: The lecture so far.
+        student_context: The general hypothesis of the student (aggregated over multiple lectures)
+        understanding_context: The specific hypothesis of student's understanding of this lectures content.
+
+    Returns:
+        A prompt for a university lecturer to continue a lecture for the next slide.
+    """
+    return (
+f"""
+You're a university lecturer whose given a lecture this point:
+<lecture>
+{lecture}
+</lecture>
+
+We're also keeping track of some extra information, to help you tailor you're lecture style. Do not mention any of this context in your response.
+
+*Context about the student you're teaching*
+<student context>
+{student_context}
+</student context>
+
+*What the student understands about this topic*
+<understanding context>
+{understanding_context}
+</understanding context>
+
+Attached is the next slide of your pdf presentation. Please continue your lecture from the exact point you left off to cover the content in this slide. Make sure your keep your lecture conversational and engaging (avoid bullet points and lists where possible). Do not end your lecturing over this slide with a question or summary line.
+"""
+    )
+
+
+def question(lecture: str, student_context: str, understanding_context: str) -> str:
+    """
+    Generate a prompt for a university lecturer to ask a question about the content of a lecture.
+
+    Args:
+        lecture: The lecture so far.
+        student_context: The general hypothesis of the student (aggregated over multiple lectures)
+        understanding_context: The specific hypothesis of student's understanding of this lectures content.
+
+    Returns:
+        A prompt for a university lecturer to ask a question about the content of a lecture.
+    """
+    return (
+f"""
+Below, we have a lecture that is being delivered to a student and a hypothesis of what we believe we know about the student.
+
+<lecture>
+{lecture}
+</lecture>
+
+<student hypothesis>
+{student_context}
+</student hypothesis>
+
+<understanding hypothesis>
+{understanding_context}
+</understanding hypothesis>
+
+Ask the student a question about the content discussed in the later parts of the lecture that will help provide insight into their level of understanding. Make the question have a short written response. Only ask the question, nothing else.
+"""
+    )
+
+
+def answer_feedback(question: str, answer: str) -> str:
+    """
+    Generate a prompt for a university lecturer to provide feedback on a student's answer to a question.
+    NOTE: This prompt expects the use of structured outputs.
+
+    Args:
+        question: The question that was asked.
+        answer: The student's answer to the question.
+
+    Returns:
+        A prompt for a university lecturer to provide feedback on a student's answer to a question.
+    """
+    return (
+f"""
+Analyse the correctness of the following student's answer to a question, and provide a brief summary addressed to them on what they did well and what they could improve on.
+
+<question>
+{question}
+</question>
+
+<answer>
+{answer}
+</answer>
+"""
+    )
